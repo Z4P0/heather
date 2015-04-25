5434409016265bd81db71c2eb5abb7d56198a5d1
luis rosario <lrosario101@gmail.com>
2015-04-25 14:48:26 -0400

    images and working on the homepage header thing

--- src/js/heather/init.js
+++ src/js/heather/init.js
@@ -5,6 +5,17 @@
         tag: 'heather.',
         init: function() {
             console.log(this.tag);
+
+            function update_header() {
+            }
+
+            // setup the header thing
+            $('.overview p').each(function (i, p) {
+                var $p  = $(p);
+                $p.hover(function (event) {
+                    console.log($p.attr('data-image-filename'));
+                });
+            });
         }
     };
 
