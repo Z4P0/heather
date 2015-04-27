e7791374620cd942338e781c1941b37573b9dc7d
luis rosario <lrosario101@gmail.com>
2015-04-27 17:26:01 -0400

    stuffs

--- src/js/heather/init.js
+++ src/js/heather/init.js
@@ -9,11 +9,22 @@
             function update_header() {
             }
 
+            // var $heather_image = $('#heather-image');
+            var $h1 = $('#h1');
+
+            // $h1.css('background', 'tomato');
             // setup the header thing
             $('.overview p').each(function (i, p) {
                 var $p  = $(p);
-                $p.hover(function (event) {
-                    console.log($p.attr('data-image-filename'));
+                $p.mouseover(function (event) {
+                    // console.log($h1);
+                    // console.log($p.attr('data-image-filename'));
+                    // $heather_image.attr('src', $p.attr('data-image-filename'));
+                    $h1.css('background', $p.attr('data-image-filename'));
+                    $h1.attr('style', 'background-image: url('+$p.attr('data-image-filename')+');');
+                    // $h1.css({
+                    //     'background-image': $p.attr('data-image-filename')
+                    // });
                 });
             });
         }
